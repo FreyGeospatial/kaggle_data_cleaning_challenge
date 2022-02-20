4b5b62d1b3a1e2c4957a94d63b35be7c722ee667
Jordan Frey <jfrey@Jordans-MacBook-Pro.local>
2022-02-20 13:29:05 -0500

    finds the number of NaN values in pd dataframe

--- cleaning.py
+++ cleaning.py
@@ -6,15 +6,24 @@ This is a temporary script file.
 """
 
 import pandas as pd
+import numpy as np
+import math
+
 nfl = pd.read_csv("NFL Play by Play 2009-2017 (v4).csv", nrows = 1)
 len(list(nfl.columns)) # 102 columns
 
-# read in all columns as strings to prevent pandas from guessing the wrong data types
-nfl = pd.read_csv("NFL Play by Play 2009-2017 (v4).csv", dtype = str)
-nfl.head
+nfl = pd.read_csv("NFL Play by Play 2009-2017 (v4).csv")
+nfl.head # we can see some NaN values
 
 permits = pd.read_csv("Building_Permits.csv", nrows = 1)
 len(list(permits.columns)) # 43
-permits = pd.read_csv("Building_Permits.csv", dtype = str)
+permits = pd.read_csv("Building_Permits.csv")
 permits.head
 
+# find which cells are NULL
+nfl.isna() # alias for .isnull(), but most reminiscent of R
+permits.isna()
+
+# we can get a count for each column's NA/NULL (NaN values), though
+nfl.isna().sum()
+permits.isna().sum()
